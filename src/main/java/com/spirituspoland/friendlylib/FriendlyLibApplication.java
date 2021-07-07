5a954949746a9ff478731bdada96d06ee623c6e0
SpiritusPoland <lkszmalinowski@gmail.com>
2021-07-07 02:55:14 +0200

    Created project

---
+++ src/main/java/com/spirituspoland/friendlylib/FriendlyLibApplication.java
@@ -0,0 +1,13 @@
+package com.spirituspoland.friendlylib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendlyLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendlyLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spirituspoland/friendlylib/FriendlyLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spirituspoland.friendlylib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendlyLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
